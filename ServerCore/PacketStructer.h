fae8031630d17af5e88d734f936228bcf53fd412
sungone <sungwon000604@gmail.com>
2024-11-07 19:14:56 +0900

    플레이어 블록 패킷 구조화
    struct ClientInfoPacket
    struct BlockRamdomSeedPacket

--- ServerCore/PacketStructer.h
+++ ServerCore/PacketStructer.h
@@ -1,6 +1,5 @@
 #pragma once
 
-
 //어떠한 데이터를 보낼것인지.
 enum CMDCODE
 {
@@ -10,15 +9,36 @@ enum CMDCODE
 	ChattingData, // 채팅에 대한정보
 	MatcingStartReady, //클라이언트에서 Matching 준비완료가 됬다는 신호 / 서버에서 3명이 되서 Matching 이 준비가 완료됬다는 신호
 	MatchingCancle // 클라이언트에서 Matching 을 취소하겠다는 신호를 서버에게 보냄.
-	
 	//필요한것을 추가할예정
 };
 
 struct MYCMD
 {
-	int Code=0;			//명령코드
-	int Size=0;			//질실적인 내용부의 데이터 크기
+	int Code = 0;			//명령코드
+	int Size = 0;			//질실적인 내용부의 데이터 크기
 	int ClientID = 0;
 };
+
+// 플레이어 위치 정보 패킷
+struct ClientInfoPacket
+{
+	float pos_x;
+	
+	float color_r;
+	float color_g;
+	float color_b;
+	
+	float scale_x;
+	float scale_y;
+};
+
+// 블록을 생성할 때 사용하는 랜덤 시드값
+struct BlockRamdomSeedPacket
+{
+	int random_seed[36][3];
+};
+
+
+
 
 
