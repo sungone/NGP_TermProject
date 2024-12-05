20eff16415707b2b9ca35c810ecde081a10b5382
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2024-12-05 12:49:33 +0900

    꼬인거 fix

---
+++ ClientTest/pch.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#pragma comment(lib, "ServerCore.lib")
+#include "ServerPch.h"
--- ServerCore/GlobalMacro.h
+++ ServerCore/GlobalMacro.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define PortNum 25000
-#define IPAddress "192.168.121.199"//121.170.7.163  121.170.7.163
+#define IPAddress "127.0.0.1"//121.170.7.163  121.170.7.163
 //#define IPAddress "211.176.102.88"//121.170.7.163
